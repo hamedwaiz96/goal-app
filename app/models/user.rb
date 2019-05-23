6dfc19d30f2f744cfd6516feb148b2d2c86be18b
Hamed Waiz <hamedw96@mgmail.com>
2019-05-22 21:16:30 -0700

    Finish User Model

---
+++ app/models/user.rb
@@ -0,0 +1,40 @@
+class User < ApplicationRecord
+  validates :email, presence: true, uniqueness: true
+  validates :password_digest, presence: { message: "Invalid Password" }
+  validates :session_token, presence: true, uniqueness: true
+  validates :password, length: { minimum: 6, allow_nil: true}
+
+  attr_reader :password
+
+  after_initialize :ensure_session_token
+
+  def generate_session_token
+    SecureRandom::urlsafe_base64(16)
+  end
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by(:email => email)
+    return user if user && is_password?(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def reset_session_token!
+    self.session_token = generate_session_token
+    self.save!
+    self.session_token
+  end
+
+
+  private
+  def ensure_session_token
+    self.session_token ||= generate_session_token
+  end
+end
