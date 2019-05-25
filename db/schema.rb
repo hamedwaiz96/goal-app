41bf0939e32f7af37b773ff8c329dbc6c226158a
Hamed Waiz <hamedw96@mgmail.com>
2019-05-24 23:14:05 -0700

    Delete Tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_25_041426) do
+ActiveRecord::Schema.define(version: 2019_05_25_061304) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "goal_comments", force: :cascade do |t|
-    t.integer "goal_id", null: false
-    t.text "body", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["goal_id"], name: "index_goal_comments_on_goal_id"
-  end
 
   create_table "goals", force: :cascade do |t|
     t.string "content", null: false
@@ -32,16 +24,6 @@ ActiveRecord::Schema.define(version: 2019_05_25_041426) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_goals_on_user_id"
-  end
-
-  create_table "user_comments", force: :cascade do |t|
-    t.integer "author_id", null: false
-    t.text "body", null: false
-    t.integer "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["author_id"], name: "index_user_comments_on_author_id"
-    t.index ["user_id"], name: "index_user_comments_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
